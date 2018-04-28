b18c8612c1addf0671481d761b17ca1b15516037
Stefan Exner <stex@sterex.de>
2018-04-28 19:03:34 +0200

    initial non-failing spec commit

--- spec/petra/rails_spec.rb
+++ spec/petra/rails_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Petra::Rails do
   it 'has a version number' do
     expect(Petra::Rails::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
